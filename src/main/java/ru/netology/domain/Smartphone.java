03444470b9f5d1adde82b57ca1fd33a15a9d8618
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-05-04 00:34:03 +0500

    Smartphone class updated

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -1,12 +1,18 @@
 package ru.netology.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode()
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class Smartphone {
+public class Smartphone extends Product{
     private String producer;
+
+    public Smartphone() {
+    }
+
+    public Smartphone(int id, String name, int price, String producer) {
+        super(id, name, price);
+        this.producer = producer;
+    }
 }
