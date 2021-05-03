c1b9b6a0ab9ecfca346ee79dc0b1956b7c844f00
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-05-04 00:30:19 +0500

    Smartphone class added

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode()
+@Data
+public class Smartphone {
+    private String producer;
+}
